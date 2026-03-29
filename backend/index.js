24abd60bb1ec43f3a09daf30ff0c02a2f2d1e16d
it17059732 <43740847+it17059732@users.noreply.github.com>
2026-03-29 23:36:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
 
+const express = require("express");
+const { Client } = require("pg");
+
+const app = express();
+
+const client = new Client({
+  host: "database-service",
+  user: "user",
+  password: "pass",
+  database: "mydb",
+  port: 5432,
+});
+
+client.connect();
+
+app.get("/", async (req, res) => {
+  const result = await client.query("SELECT NOW()");
+  res.send("Backend + DB connected: " + result.rows[0].now);
+});
+
+app.listen(5000, () => {
+  console.log("Backend running on port 5000");
+});
